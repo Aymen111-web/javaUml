130ec96aaceb13c13239a6bc2e341bea2cef2750
Aymen111-web <aymimanmoh8@gmail.com>
2025-05-14 09:23:42 +0300

    first commit

---
+++ Desktop/nootbook/Account.java
@@ -0,0 +1,22 @@
+import java.util.Vector;
+
+public class Account {
+
+  public Integer accountName;
+
+  public Integer AccountId;
+
+  private string Sex;
+
+    public Vector  my;
+
+  public void withdraw() {
+  }
+
+  public void deposit() {
+  }
+
+  public void transfer() {
+  }
+
+}
